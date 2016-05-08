31541c4f56863e58a87765686b519a31127b04c8
Dave <davem220@gmail.com>
2016-05-08 13:09:17 -0400

    feat:add my api script

--- task1.py
+++ task1.py
@@ -1,92 +1,44 @@
 import requests
 
-answer = input("Enter address or lat/log coordinates: ")
+def address(address):
+	url = 'https://developer.nrel.gov/api/utility_rates/v3.json' 
+	api_key = '&api_key=zlllkbtTApEZhSsAewdMglmE9kf7lrPP3nnVN75x&format=JSON'
+	api_url = url + '?address=' + address + api_key
+	return api_url
 
-if answer == 'address':
-	address = input("What is your address?")
-	address.strip().split()
-	api_url = 'https://developer.nrel.gov/api/utility_rates/v3.json' 
-			  + '?address=' + address 
-			  + '&api_key=zlllkbtTApEZhSsAewdMglmE9kf7lrPP3nnVN75x&format=JSON'
-else:
-	print("If you need to look up your address for lat/lon, you can go to the following url: mygeoposition.com")
-	lat = float(input("Enter your latitude(number must be between -90 and 90): "))
-	if lat < -90 or lat > 90:
-		print("That number is out of range, please try again")
-		lat = float(input("Enter your latitude(number must be between -90 and 90): "))
-	lon = float(input("Enter you longitude(number must be between -180 and 180):"))
-	if lon < -180 or lon > 180:
-		print("That number is out of range, please try again")
-		lon = float(input("Enter your longitude(number must be between -180 and 180): "))
-	api_url = 'https://developer.nrel.gov/api/utility_rates/v3.json' + '?lat=' + str(lat) + '&lon=' + str(lon) + '&api_key=zlllkbtTApEZhSsAewdMglmE9kf7lrPP3nnVN75x&format=JSON'
+def lat_lon(lat, lon):
+	url = 'https://developer.nrel.gov/api/utility_rates/v3.json' 
+	api_key = '&api_key=zlllkbtTApEZhSsAewdMglmE9kf7lrPP3nnVN75x&format=JSON'
+	api_url = url + '?lat=' + str(lat) + '&lon=' + str(lon) + api_key
+	return api_url
 
-r = requests.get(api_url)
+def get_all_info_from_api():
+	answer = input("Would you like to enter an address or lat/lon?[address/lat_lon]")
+	if answer == 'address':
+		return address(input("What is your address: "))
+	else:
+		print("You can get you lat/lon coordinates from mygeoposition.com")
+		lat = float(input("Enter your latitude(Enter between -90 and 90): "))
+		if lat < -90 or lat > 90:
+			print("ALERT, That number is out of range, please try again")
+			lat = float(input("Enter your latitude(Enter between -90 and 90): "))
+		lon = float(input("Enter your longitude(Enter between -180 and 180):"))
+		if lon < -180 or lon > 180:
+			print("ALERT, That number is out of range, please try again")
+			lon = float(input("Enter your longitude(Enter between -180 and 180): "))
+		return lat_lon(lat, lon)
+
+get_info = get_all_info_from_api()
+r = requests.get(get_info)
 print(r.text)
 
+def enter_another_address():
+	print("Would you like to enter another address or lat/lon coordinates?[y/n]")
+	if input() == 'y' or input() == 'yes':
+		get_info = get_all_info_from_api()
+		r = requests.get(get_info)
+		print(r.text)
+	else:
+		print("No problem, come back again anytime!")
 
-def address():
-	address = input("What is your address?")
-	address.strip().split()
-	api_url = 'https://developer.nrel.gov/api/utility_rates/v3.json' 
-			  + '?address=' + address 
-			  + '&api_key=zlllkbtTApEZhSsAewdMglmE9kf7lrPP3nnVN75x&format=JSON'
-	return api_url
-
-def lat_lon():
-	print("If you need to look up your address for lat/lon, you can go to the following url: mygeoposition.com")
-	lat = float(input("Enter your latitude(number must be between -90 and 90): "))
-	if lat < -90 or lat > 90:
-		print("That number is out of range, please try again")
-		lat = float(input("Enter your latitude(number must be between -90 and 90): "))
-	lon = float(input("Enter you longitude(number must be between -180 and 180):"))
-	if lon < -180 or lon > 180:
-		print("That number is out of range, please try again")
-		lon = float(input("Enter your longitude(number must be between -180 and 180): "))
-	api_url = 'https://developer.nrel.gov/api/utility_rates/v3.json' + '?lat=' + str(lat) + '&lon=' + str(lon) + '&api_key=zlllkbtTApEZhSsAewdMglmE9kf7lrPP3nnVN75x&format=JSON'
-	return api_url
-
-
-address = address()
-lat_lon = lat_lon()
-
-def question():
-	answer = input("Enter address or lat/log coordinates: ")
-	if answer == 'address':
-		return address
-	else: 
-		return lat_lon
-	go_again = input("Would you like to enter another address? [y/n]")
-	if go_again == 'y' or go_again == 'yes':
-		return question()
-	else: 
-		pass
-
-#to use later:
-
-'''import urllib.parse
-
-api_url = 'https://developer.nrel.gov/api/utility_rates/v3.json?'
-data = {
-    "lat": lat,
-    "lon": lon,
-    "api_key": "zlllkbtTApEZhSsAewdMglmE9kf7lrPP3nnVN75x",
-    "format": "JSON"
-}
-api_url += urllib.parse.urlencode(data)
-
-def response_in_range(question, low, high, type=float):
-    answer = get_response(question, type)
-    if low > answer > high:
-        print("Pick a number between: {} - {}".format(low, high))
-        return response_in_range(question, low, high, type)
-    return answer
-
-def get_response(question, type):
-    try:
-        return type(input(question))
-    except ValueError:
-        print("Invalid Input!")
-        return get_response(question, type)
-
-
-lat = response_in_range("Enter Latitude", -90, 90)'''
+enter_another_address()
